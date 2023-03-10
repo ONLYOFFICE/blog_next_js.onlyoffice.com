92d51ad6c4f2db038c8423c7c1cfc3052377506a
Denis <denistyan16@gmail.com>
2023-03-10 18:03:50 +0500

    feat: add breadcrumbs

---
+++ components/screens/common/breadcrumbs/index.js
@@ -0,0 +1,38 @@
+import StyledBreadcrumbs from "./styled-breadcrumbs";
+import InternalLink from "@components/common/internal-link";
+
+const Breadcrumbs = ({ t, data, isPostContent, isAuthorContent, isInThePressContent, isCategoryContent, isSearchContent, searchQuery, ...rest }) => {
+  return (
+    <StyledBreadcrumbs {...rest}>
+      <li>
+        <InternalLink href="/">{t("Blog")}</InternalLink>
+      </li>
+      &ensp;/&ensp;
+      {
+        isPostContent ?
+          <>
+            <li>
+              {data.map(({node}) => {
+                return <span key={node.id}><InternalLink href={`/category/${node?.slug}`}>{node.name}</InternalLink>&ensp;/&ensp;</span>
+              })}
+            </li>
+            <li>
+              {t("Onlyoffice")}
+            </li>
+          </> 
+        :
+          isAuthorContent ? <li>{data.edges[0]?.node.author?.node?.name}</li> 
+        :
+          isInThePressContent ? <li>{t("ONLYOFFICE IN THE PRESS")}</li> 
+        :
+          isCategoryContent ? <li>{data.edges[0]?.node.categories?.nodes[0]?.name}</li>
+        :
+          isSearchContent ? <li>{`${t("Search results for")} '${searchQuery === undefined ? "" : searchQuery}'`}</li> 
+        : 
+          null
+      }
+    </StyledBreadcrumbs>
+  );
+};
+
+export default Breadcrumbs;
---
+++ components/screens/common/breadcrumbs/styled-breadcrumbs.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const StyledBreadcrumbs = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  color: #444444;
+
+  a {
+    display: inline-flex;
+    text-decoration: none;
+
+    span {
+      font-weight: 600;
+      font-size: 13px;
+      line-height: 133%;
+      letter-spacing: 0.04em;
+      text-transform: uppercase;
+      text-decoration: none;
+      color: #444444;
+
+      &:hover {
+        text-transform: uppercase;
+        text-decoration: underline;
+        color: #444444;
+      }
+    }
+  }
+`;
+
+export default StyledBreadcrumbs;
