46ac98529b5a5a01ebab6e0a641f8d590ccbcfb4
Denis <denistyan16@gmail.com>
2023-04-06 14:50:12 +0500

    fix(recent-posts): fix date

--- components/screens/common/widgets/recent-posts/index.js
+++ components/screens/common/widgets/recent-posts/index.js
@@ -1,27 +1,30 @@
 import StyledRecentPosts from "./styled-recent-posts";
-import Moment from "moment";
+import dynamic from "next/dynamic";
 import Heading from "@components/common/heading";
 import InternalLink from "@components/common/internal-link";
 
-const RecentPosts = ({ t, data }) => {
+const RecentPosts = ({ t, currentLanguage, data }) => {
+  const isRecentPosts = true;
+
+  const DateFormat = dynamic(() => import("@components/screens/common/date-format"), {
+    ssr: false
+  });
+
   return (
     <StyledRecentPosts>
-      <Heading level={4}>{t("Recent Posts")}</Heading>
+      <Heading level={4}>{t("Recent posts")}</Heading>
 
       <ul>
         {data.edges.map(({node}) => (
           <li className="post" key={node.id}>
             <InternalLink className="post-img" href={node?.uri}>
-              {
-                node.featuredImage?.node.sourceUrl ?
-                  <img src={node.featuredImage?.node?.sourceUrl} alt={node?.title} />
-                :
-                  <img src={node?.firstImgPost} alt={node?.title} />
-              }
+              <img src={node.featuredImage?.node.sourceUrl ? node.featuredImage?.node?.sourceUrl : node?.firstImgPost} alt={node?.title} />
             </InternalLink>
             <div className="post-body">
               <InternalLink className="post-title" href={node?.uri}>{node.title}</InternalLink>
-              <div className="post-date">{Moment(node.date).format("MMMM D, y")}</div>
+              <div className="post-date">
+                <DateFormat currentLanguage={currentLanguage} data={node.date} format="MMMM D, y" isRecentPosts={isRecentPosts} />
+              </div>
             </div>
           </li>
         ))}
