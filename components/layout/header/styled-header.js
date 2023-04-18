8b42e0a99a53a20260734411dd72db89856d19ae
Denis <denistyan16@gmail.com>
2023-04-18 14:51:13 +0500

    fix(header): fix styles

--- components/layout/header/styled-header.js
+++ components/layout/header/styled-header.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
+import { device } from "@components/utils/devices";
 
 const StyledHeader = styled.header`
-position: sticky;
-top: 0px;
-left: 0;
-width: 100%;
-z-index: 1000;
+  position: sticky;
+  top: 0px;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+
+  @media ${device.laptop} {
+    overflow-x: hidden;
+  }
 `;
 
 export default StyledHeader;
