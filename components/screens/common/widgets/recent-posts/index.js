4ffc885b0e3cee1ea0ea06c8f188b8ddb9e22176
Svetlana Lisina <vyacheslav.belov@onlyoffice.com>
2023-04-29 15:47:10 +0300

    Revert "Revert "fix: fix url for images""
    This reverts commit 880684038cb4f8d0d229c7257a57483e231e4f54.

--- components/screens/common/widgets/recent-posts/index.js
+++ components/screens/common/widgets/recent-posts/index.js
@@ -5,6 +5,8 @@ import InternalLink from "@components/common/internal-link";
 
 const RecentPosts = ({ t, currentLanguage, data }) => {
   const isRecentPosts = true;
+  const currentImgUrl = "https://wpblog.teamlab.info/wp-content/";
+  const cdnImgUrl = "https://static-blog.teamlab.info/wp-content/";
 
   return (
     <StyledRecentPosts>
@@ -14,7 +16,7 @@ const RecentPosts = ({ t, currentLanguage, data }) => {
         {data.edges.map(({node}) => (
           <li className="post" key={node.id}>
             <InternalLink className="post-img" href={node?.uri}>
-              <img src={node.featuredImage?.node.sourceUrl ? node.featuredImage?.node?.sourceUrl : node?.firstImgPost} alt={node?.title} />
+              <img src={node.featuredImage?.node.sourceUrl ? node.featuredImage?.node.sourceUrl.replace(currentImgUrl, cdnImgUrl) : node?.firstImgPost.replace(currentImgUrl, cdnImgUrl)} alt={node?.title} />
             </InternalLink>
             <div className="post-body">
               <InternalLink className="post-title" href={node?.uri}>{node.title}</InternalLink>
