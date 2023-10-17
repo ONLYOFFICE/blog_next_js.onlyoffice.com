f83935f73b65715395fa463d5fc8f43c58770e7f
DenisTyan <denistyan16@gmail.com>
2023-10-17 15:30:35 +0500

    fix(post): fix alternate links in post

--- components/common/language-selector/items-list.js
+++ components/common/language-selector/items-list.js
@@ -28,7 +28,7 @@ const ItemsList = ({ isOpen, onCloseSelector, postUri, isPostContent }) => {
             <StyledItem className="language-item" key={language.locale}>
               <Link
                 className={`language-item-link ${language.shortKey}`}
-                href={postUri[language.locale] && `${postUri[language.locale].replace("https://wpblog.onlyoffice.com", "").replace("https://wpblog.teamlab.info", "")}` || "/"}
+                href={postUri[language.locale] && `${postUri[language.locale].split("/").slice(3).join("/")}` || "/"}
                 locale={language.shortKey}
                 onClick={onCloseSelector}
               ></Link>
