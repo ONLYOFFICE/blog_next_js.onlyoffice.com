0d5ecd42b4764052c2688c0d562f6cfad561c16e
DenisTyan <denistyan16@gmail.com>
2023-12-06 16:26:00 +0500

    fix(advent-announce): change to DocSpace 2.0

--- components/screens/heading-content/advent-announce/index.js
+++ components/screens/heading-content/advent-announce/index.js
@@ -2,28 +2,26 @@ import StyledAdventAnnounce from "./styled-advent-announce";
 
 const AdventAnnounce = ({t, locale, stateMobile }) => {
   const href = 
-    locale === "fr" ? "https://www.onlyoffice.com/blog/fr/2023/10/sortie-onlyoffice-docs-7-5" :
-    locale === "de" ? "https://www.onlyoffice.com/blog/de/2023/10/onlyoffice-docs-7-5-veroeffentlicht" :
-    locale === "es" ? "https://www.onlyoffice.com/blog/es/2023/10/disponible-onlyoffice-docs-7-5" :
-    locale === "pt-br" ? "https://www.onlyoffice.com/blog/pt-br/2023/10/onlyoffice-docs-7-5-lancado" :
-    locale === "it" ? "https://www.onlyoffice.com/blog/it/2023/10/onlyoffice-docs-7-5" :
-    locale === "ja" ? "https://www.onlyoffice.com/blog/ja/2023/10/onlyoffice-docs-7-5-released" :
-    locale === "zh-hans" ? "https://www.onlyoffice.com/blog/zh-hans/2023/10/onlyoffice-docs-7-5-released" : 
-    "https://www.onlyoffice.com/blog/2023/10/onlyoffice-docs-7-5-released";
+    locale === "fr" ? "https://www.onlyoffice.com/blog/fr/2023/12/onlyoffice-docspace-2-0" :
+    locale === "de" ? "https://www.onlyoffice.com/blog/de/2023/12/onlyoffice-docspace-2-0-veroeffentlicht" :
+    locale === "es" ? "https://www.onlyoffice.com/blog/es/2023/12/disponible-onlyoffice-docspace-2-0" :
+    locale === "pt-br" ? "https://www.onlyoffice.com/blog/pt-br/2023/12/onlyoffice-docspace-2-0-disponivel" :
+    locale === "it" ? "https://www.onlyoffice.com/blog/it/2023/12/onlyoffice-docspace-2-0" :
+    "https://www.onlyoffice.com/blog/2023/12/onlyoffice-docspace-2-0-released";
 
   return (
     <StyledAdventAnnounce className={`${locale} ${stateMobile ? "active": ""}`}>
       <div className="advent-announce advent-mobile-hide">
         <a href={href} target="_blank" rel="noreferrer noopener">
           <div className="advent-announce-text">
-            <b>{t("ONLYOFFICE Docs v7.5 released")}{`${locale === "zh-hans" ? "：" : locale === "ja" ? "： " : ": "}`}</b>{t("PDF Editor, automatic hyphenation, Page Breaks and tracer arrows in sheets, Screen Readers, and more")}
+            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceDesktop")}} />
           </div>
         </a>
       </div>
       <div className={`advent-announce advent-desktop-hide ${stateMobile ? "is-open": ""}`}>
         <a href={href} target="_blank" rel="noreferrer noopener">
           <div className="advent-announce-text">
-            <b>{t("ONLYOFFICE Docs v7.5 released")}</b>
+            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceMobile")}} />
           </div>
         </a>
       </div>
