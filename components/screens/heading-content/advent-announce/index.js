4983a99de7c9092f5e912686903a80de6b423d5a
DenisTyan <denistyan16@gmail.com>
2024-02-28 13:40:37 +0500

    feat(advent-announce): change to ONLYOFFICE DocSpace app for Zoom

--- components/screens/heading-content/advent-announce/index.js
+++ components/screens/heading-content/advent-announce/index.js
@@ -3,14 +3,14 @@ import InternalLink from "@components/common/internal-link";
 
 const AdventAnnounce = ({t, locale, stateMobile }) => {
   const href = 
-    locale === "fr" ? "https://www.onlyoffice.com/blog/fr/2024/01/onlyoffice-docs-8-0" :
-    locale === "de" ? "https://www.onlyoffice.com/blog/de/2024/01/onlyoffice-docs-8-0-veroeffentlicht" :
-    locale === "es" ? "https://www.onlyoffice.com/blog/es/2024/01/onlyoffice-docs-8-0" :
-    locale === "pt-br" ? "https://www.onlyoffice.com/blog/pt-br/2024/01/onlyoffice-docs-8-0" :
-    locale === "it" ? "https://www.onlyoffice.com/blog/it/2024/01/onlyoffice-docs-8-0" :
-    locale === "ja" ? "https://www.onlyoffice.com/blog/ja/2024/01/onlyoffice-docs-8-0-released" :
-    locale === "zh-hans" ? "https://www.onlyoffice.com/blog/zh-hans/2024/01/onlyoffice-docs-8-0-released" :
-    "https://www.onlyoffice.com/blog/2024/01/onlyoffice-docs-8-0-released";
+    locale === "fr" ? "https://www.onlyoffice.com/blog/fr/2024/02/application-onlyoffice-docspace-pour-zoom" :
+    locale === "de" ? "https://www.onlyoffice.com/blog/de/2024/02/onlyoffice-docspace-app-fuer-zoom-arbeiten-sie-in-videokonferenzen-an-dokumenten-zusammen" :
+    locale === "es" ? "https://www.onlyoffice.com/blog/es/2024/02/app-onlyoffice-docspace-para-zoom" :
+    locale === "pt-br" ? "https://www.onlyoffice.com/blog/pt-br/2024/02/aplicativo-onlyoffice-docspace-para-zoom" :
+    locale === "it" ? "https://www.onlyoffice.com/blog/it/2024/02/onlyoffice-docspace-per-zoom" :
+    locale === "ja" ? "https://www.onlyoffice.com/blog/ja/2024/02/onlyoffice-docspace-app-for-zoom" :
+    locale === "zh-hans" ? "https://www.onlyoffice.com/blog/zh-hans/2024/02/onlyoffice-docspace-app-for-zoom" :
+    "https://www.onlyoffice.com/blog/2024/02/onlyoffice-docspace-app-for-zoom";
 
   return (
     <StyledAdventAnnounce className={`${locale} ${stateMobile ? "active": ""}`}>
