6ebd0c12f2b102966b7781e9d059ca392abcea62
DenisTyan <denistyan16@gmail.com>
2024-04-26 11:45:28 +0500

    feat(nav): add site scroll ban, when mobile menu is open

--- components/screens/heading-content/index.js
+++ components/screens/heading-content/index.js
@@ -1,14 +1,12 @@
+import { StyledHeadingContent, GlobalStyles } from "./styled-heading-content";
+import Nav from "./menu/nav/nav";
 import { useState, useEffect } from "react";
-import Router from 'next/router';
+import Router from "next/router";
 import InternalLink from "@components/common/internal-link";
 import LanguageSelector from "@components/common/language-selector";
 import SearchArea from "@components/common/search-area";
-import Nav from './menu/nav/nav'
-import StyledHeadingContent from "./styled-heading-content";
 
 const HeadingContent = ({ t, locale, isMainContent, isSearchContent, stateMobile, setStateMobile, postUri, isPostContent }) => {
-  const [windowCheck, setWindowCheck] = useState("undefined");
-  const [windowCheckSearch, setWindowCheckSearch] = useState("undefined");
   const [searchActive, setSearchActive] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const curLang = `https://www.onlyoffice.com${
@@ -17,57 +15,43 @@ const HeadingContent = ({ t, locale, isMainContent, isSearchContent, stateMobile
     locale === "pt-br" ? "/pt" : `/${locale}`
   }`;
 
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.innerWidth <= 1024 && stateMobile) {
+      const handleClickOutside = (e) => {
+        if (!e.target.closest(".nav")) {
+          setStateMobile(false);
+        };
+      };
+
+      window.addEventListener("touchstart", handleClickOutside);
+
+      return () => {
+        window.removeEventListener("touchstart", handleClickOutside);
+      };
+    }
+  }, [stateMobile]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.innerWidth <= 1530) {
+      const handleClickOutsideSearch = (e) => {
+        if (searchActive && !e.target.closest(".navbar .search_area")) {
+          setSearchActive(false);
+        }
+      };
+
+      window.addEventListener("touchstart", handleClickOutsideSearch);
+
+      return () => {
+        window.removeEventListener("touchstart", handleClickOutsideSearch);
+      };
+    }
+  }, [searchActive]);
+
   const handleSearchFormSubmit = async (e) => {
     e.preventDefault();
 
     Router.push(`/search?s=${searchQuery}`);
     return null;
-  };
-
-  useEffect(() => {
-    if (typeof window !== windowCheck) {
-      setWindowCheck(window.innerWidth <= 1050);
-    }
-  }, [windowCheck]);
-
-  useEffect(() => {
-    window.addEventListener("touchstart", handleClickOutside);
-    return () => {
-      window.removeEventListener("touchstart", handleClickOutside);
-    };
-  }, [stateMobile]);
-
-  useEffect(() => {
-    if (typeof window !== windowCheckSearch) {
-      setWindowCheckSearch(window.innerWidth <= 1530);
-    }
-  }, [windowCheckSearch]);
-
-  useEffect(() => {
-    window.addEventListener("touchstart", handleClickOutsideSearch);
-    return () => {
-      window.removeEventListener("touchstart", handleClickOutsideSearch);
-    };
-  }, [searchActive]);
-
-  const toggleMobile = () => {
-    setStateMobile(true);
-  };
-
-  const onCloseMenu = () => {
-    setStateMobile(false);
-  };
-
-  const handleClickOutside = (e) => {
-    if (windowCheck && stateMobile && !e.target.closest(".nav")) {
-      onCloseMenu();
-    }
-  };
-
-  const handleClickOutsideSearch = (e) => {
-    if (windowCheckSearch && searchActive && !e.target.closest(".navbar .search_area")) {
-      setSearchActive(false);
-    }
   };
 
   const onClickSearch = () => {
@@ -77,21 +61,22 @@ const HeadingContent = ({ t, locale, isMainContent, isSearchContent, stateMobile
   };
 
   return (
-    <StyledHeadingContent className={`navbar ${stateMobile ? "is-open" : ""}`} onMouseLeave={onCloseMenu}>
-      <button onClick={toggleMobile} className="nav-btn-mobile"></button>
+    <StyledHeadingContent onMouseLeave={() => setStateMobile(false)} className={`navbar ${stateMobile ? "is-open" : ""}`}>
+      <GlobalStyles stateMobile={stateMobile} />
+      <button onClick={() => setStateMobile(true)} className="nav-btn-mobile"></button>
       <InternalLink className="nav-item-logo" href={curLang}>
-        <img src="/blog/images/logo/logo.svg" alt="logo"/>
+        <img src="/blog/images/logo/logo.svg" alt="logo" />
       </InternalLink>
       <div className="overlay"></div>
       <Nav locale={locale} t={t} />
-      {isMainContent || isSearchContent !== true && 
-        <SearchArea 
-          onClick={onClickSearch} 
-          searchQuery={searchQuery} 
-          setSearchQuery={setSearchQuery} 
+      {isMainContent || isSearchContent !== true &&
+        <SearchArea
+          onClick={onClickSearch}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
           handleSearchFormSubmit={handleSearchFormSubmit}
-          searchActive={searchActive} 
-          t={t} 
+          searchActive={searchActive}
+          t={t}
           placeholder={t("Search blog")}
         />
       }
