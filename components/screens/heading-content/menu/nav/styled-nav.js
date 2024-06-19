0f415591bac5185b14c69173e9527f93b0e557dc
DenisTyan <denistyan16@gmail.com>
2024-06-19 17:48:19 +0500

    fix(nav): fix styles

--- components/screens/heading-content/menu/nav/styled-nav.js
+++ components/screens/heading-content/menu/nav/styled-nav.js
@@ -335,7 +335,6 @@ const StyledNav = styled.nav`
     }
 
     .menu-wrapper-bottom {
-      margin: 0 32px;
       justify-content: flex-end;
 
       .menu-link {
@@ -674,7 +673,7 @@ const StyledNav = styled.nav`
     display: flex;
     justify-content: space-between;
     border-top: 1px solid #E2E2E2;
-    padding: 12px 24px;
+    padding: 15px 24px 16px 24px;
 
     @media screen and ${device.laptop} {
       justify-content: initial;
