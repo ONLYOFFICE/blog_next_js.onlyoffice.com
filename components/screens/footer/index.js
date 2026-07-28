2a3d8e4e4345d2e4c5a869f6ba8cc92c1d8ac923
DenisTyan <denistyan16@gmail.com>
2026-07-28 14:29:23 +0500

    fix: fix postUri

--- components/screens/footer/index.js
+++ components/screens/footer/index.js
@@ -14,7 +14,7 @@ const Footer = ({ locale, postUri }) => {
         name: language.longKey,
         href:
           router.pathname === "/[...post]"
-            ? postUri[language.locale] && postUri[language.locale].split("/").slice(3).join("/") || "/"
+            ? postUri[language.locale] && postUri[language.locale] || "/"
             : router.pathname === "/onlyoffice-in-the-press" || router.pathname === "/search"
             ? router.asPath
             : "/"
